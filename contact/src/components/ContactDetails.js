42f6c1a51d3014b986a41ee50df91d84a248acb5
prograpper <barkbaek@gmail.com>
2019-12-23 21:05:54 -0500

    Change component life cycle

--- contact/src/components/ContactDetails.js
+++ contact/src/components/ContactDetails.js
@@ -14,6 +14,7 @@ export default class ContactDetails extends React.Component {
         this.handleToggle = this.handleToggle.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handleEdit = this.handleEdit.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
     }
 
     handleToggle() {
@@ -41,6 +42,12 @@ export default class ContactDetails extends React.Component {
         this.props.onEdit(this.state.name, this.state.phone);
     }
 
+    handleKeyPress(e) {
+        if (e.charCode === 13) {
+            this.handleToggle();
+        }
+    }
+
     render() {
         const details = (
             <div>
@@ -57,7 +64,8 @@ export default class ContactDetails extends React.Component {
                         name="name"
                         placeholder="name"
                         value={this.state.name}
-                        onChange={this.handleChange}/>
+                        onChange={this.handleChange}
+                    />
                 </p>
                 <p>
                     <input
@@ -65,7 +73,9 @@ export default class ContactDetails extends React.Component {
                         name="phone"
                         placeholder="phone"
                         value={this.state.phone}
-                        onChange={this.handleChange}/>
+                        onChange={this.handleChange}
+                        onKeyPress={this.handleKeyPress}
+                    />
                 </p>
             </div>
         );
