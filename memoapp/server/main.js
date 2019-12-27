a2639e854f7025329a7bfcaa7f2225328d58a6ee
prograpper <barkbaek@gmail.com>
2019-12-27 15:07:54 -0500

    Add memoapp

---
+++ memoapp/server/main.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import path from 'path';
+
+import morgan from 'morgan'; // HTTP REQUEST LOGGER
+import bodyParser from 'body-parser'; // PARSE HTML BODY
+
+import mongoose from 'mongoose';
+import session from 'express-session';
+
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+/* mongodb connection */
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => { console.log('Connected to mongodb server'); });
+// mongoose.connect('mongodb://username:password@host:port/database=');
+mongoose.connect('mongodb://barkbaek:qkdpr88bb!B@127.0.0.1:26917/barkbaek');
+
+/* use session */
+app.use(session({
+    secret: 'CodeLab1$1$234',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use('/', express.static(path.join(__dirname, './../public')));
+
+app.get('/hello', (req, res) => {
+    return res.send('Hello CodeLab');
+});
+
+app.listen(port, () => {
+    console.log('Express is listening on port', port);
+});
